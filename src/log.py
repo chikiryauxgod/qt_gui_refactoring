799f5f6ecba8053eab774bb7187c6caa12867f76
faweddd <igormaluh010@mail.ru>
2026-01-17 22:32:29 +0500

    add log.py stubs for qt_interface.py

---
+++ src/log.py
@@ -0,0 +1,10 @@
+import sys
+
+class Log:
+    def __call__(self, message, queue=None, file=None):
+        # вывод в указанный поток или в stdout по умолчанию
+        if file is None:
+            file = sys.stdout
+        print(f"LOG: {message}", file=file)
+        if queue is not None:
+            queue.put(message)
